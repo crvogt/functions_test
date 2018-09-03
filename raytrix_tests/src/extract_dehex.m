9c5bbb50720ae4cba22cbf9ed47b74a40cc04da5
crvogt <crvogt26@gmail.com>
2018-09-03 09:05:48 +0100

    added dehex stuff

--- raytrix_tests/src/extract_dehex.m
+++ raytrix_tests/src/extract_dehex.m
@@ -3,23 +3,28 @@ close all;
 clear all;
 
 %Load image
-[img, daMap] = imread('processed.png', 'png');
-figure()
+[img, daMap] = imread('../data/processed.png', 'png');
 
-% if isempty(daMap)
-% 	warndlg('Map is empty...');
-% else
-% 	helpdlg('Map not empty!');
-% end
-
-imshow(img, daMap)
-% img
-%Load data
-A = importdata('mxa_details.txt');
+A = importdata('../data/mxa_details.txt');
 lens0 = A(1:2929,1:2);
 lens1 = A(2930:5858, 1:2);
 lens2 = A(5859:8760, 1:2);
 
+figure()
+imshow(img, daMap)
+
+hold on;
+plot(lens0(1,1), lens0(1,2), '*r', 'MarkerSize', 20);
+plot(lens0(:,1), lens0(:,2), '.r', 'MarkerSize', 5);
+plot(lens1(:,1), lens1(:,2), '.r', 'MarkerSize', 5);
+plot(lens2(:,1), lens2(:,2), '.r', 'MarkerSize', 5);
+% plot(x,y,'r.', 'MarkerSize', 5);
+hold off;
+
+% test = imcrop(img, [0 0 200 200]);
+% imshow(test)
+
+% pause(1000)
 
 figure()
 hold on;
@@ -116,11 +121,62 @@ figure()
 imshow(accImage);
 
 % Get an individual lens. Effective radius ~11.5, so call it 12.
-effR = 12;
-lensImg = zeros(effR*2, effR*2, 3);
+effR = 12*1;
+numLenses = 29;
+lensImg = zeros(effR*2+1, effR*2+1, 3);
+chosenLenses = zeros(effR*2+1, effR*2+1, 3, numLenses, 'uint16');
+for counter = 30:1:30+numLenses
+	% counter=1
+	% counter
+	if (ceil(lens0(counter,2)) - lens0(counter,2)) >= 0.5
+		rowVal = floor(lens0(counter,2));
+	else 
+		rowVal = ceil(lens0(counter,2));
+	end
+	if (ceil(lens0(counter,1)) - lens0(counter,1)) >= 0.5
+		colVal = floor(lens0(counter,1));
+	else 
+		colVal = ceil(lens0(counter,1));
+	end
 
-I2 = imcrop(img,[0 0 400 800]);
-imshow(I2);
+	% rowVal = 0;
+	% lens0(1,1)
+	% lens0(1,2)
+	% colVal
+	% rowVal
+	% [rowVal-effR, colVal-effR, effR*2, effR*2]
+	% lensImg = imcrop(img,[rowVal-effR, colVal-effR, effR*2+100, effR*2+1]);
+	% testerImg = imcrop(img,[rowVal-effR, colVal-effR, effR*2, effR*2+500]);
+	lensImg = imcrop(img,[colVal-effR, rowVal-effR, effR*2, effR*2]);
+	chosenLenses(:,:,:,counter) = lensImg; 
+	if counter == 1
+		firstImg = lensImg;
+		% chosenLenses = lensImg;
+		% imshow(chosenLenses)
+		% pause(10)
+	else
+	% 	chosenLenses = cat(3,lensImg);
+	end
+	
+	% imshow(lensImg);
+	% hold on;
+	% plot(effR+1, effR+1, '.r', 'MarkerSize', 5);
+	% hold off
+	% pause(2)
+	% close all;
+end
+% figure()
+% imshow(chosenLenses(:,:,:,1));
+% figure()
+% imshow(firstImg)
+% firstImg(1,1,1)
+% chosenLenses(1,1,1,1)
+figure()
+% montage(chosenLenses, 'Size', [1 numLenses])
+% size(chosenLenses)
+montage(chosenLenses, 'Size', [1 numLenses])
+
+
 % for counter = 1:100:2000
 % 	% counter = 1000;
 
