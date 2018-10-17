6353b73221e09abea880593431ee3ad7ba40c9ec
crvogt <crvogt26@gmail.com>
2018-10-17 16:21:28 +0100

    added more to depth from de4focus

--- python_depth_warp/depth_from_defoc.py
+++ python_depth_warp/depth_from_defoc.py
@@ -1,52 +1,118 @@
 import numpy as np
 import time
 import cv2
+from scipy.interpolate import RectBivariateSpline
+
+
+def display_lf(imgArray):
+    for iVal in range(1, 16):
+        for jVal in range(1, 16):
+            cv2.imshow("img", imgArray[:, :, :, jVal-1, iVal-1])
+            cv2.waitKey(100)
+
+
+def interp2(grayImg, moveX, moveY):
+    height, width = grayImg.shape
+    # print(moveX.size)
+    # print(height, width)
+    # time.sleep(20)
+    x = np.arange(0, width, 1)
+    y = np.arange(0, height, 1)
+
+    print("moveX: " + str(moveX))
+    print("moveY: " + str(moveY))
+
+    interped = RectBivariateSpline(y, x, grayImg)
+
+    x2 = np.linspace(moveX, width + moveX, width)
+    y2 = np.linspace(moveY, height + moveY, height)
+
+    Z = interped(y2, x2)
+    Z = Z.astype(np.uint8)
+
+    # Going to leave this for now, might have to revisit
+    # Z[0:height, 0:int(moveY)] = 0
+    # Z[0:int(moveX), 0:width] = 0
+
+    cv2.imshow("sheared lf", Z)
+    cv2.imwrite("shearTests/"+str(moveX)+"_"+str(moveY)+".png", Z)
+    cv2.waitKey(100)
+
+    return Z
+
 
 nRows = 15
 nCols = 15
 
-xx = np.linspace(0, 1, nCols) # Return numbers over evenly spaced interval
+# Load light field SAIs
+lfLocation = "/home/carson/libs/SAI_output/"
+
+LF = np.zeros((101, 101, 3, nRows, nCols), dtype=np.uint8)
+
+terCount = 0
+for xTer in range(1, 16):
+    for yTer in range(1, 16):
+        lfName = ""
+        if xTer > 9 and yTer > 9:
+            lfName = "CRV_" + str(xTer) + "_" + str(yTer) + ".png"
+        elif xTer < 10 and yTer < 10:
+            lfName = "CRV_0" + str(xTer) + "_0" + str(yTer) + ".png"
+        elif xTer < 10 and yTer > 9:
+            lfName = "CRV_0" + str(xTer) + "_" + str(yTer) + ".png"
+        else:
+            lfName = "CRV_" + str(xTer) + "_0" + str(yTer) + ".png"
+
+        lfName = lfLocation + lfName
+        src = cv2.imread(lfName, 1)
+
+        LF[:, :, :, yTer-1, xTer-1] = src
+
+        terCount = terCount + 1
+
+xx = np.linspace(0, 1, nCols)   # Return numbers over evenly spaced interval
 yy = np.linspace(0, 1, nRows)
 
 X, Y = np.meshgrid(xx, yy)
 
+# Gives us the coordinates of all of the images
 X = np.asarray(X).reshape(-1)
 Y = np.asarray(Y).reshape(-1)
-# X.reshape()
-# print(X.size)
-# print(X)
-# print(Y[0])
+# Center image
+cxInd = (X.size-1)/2
+cyInd = (Y.size-1)/2
+cX = X[cxInd]
+cY = Y[cyInd]
+# Get delta between images and center image
+dX = X - cX
+dY = Y - cY
 
-# Load light field SAIs
-lfLocation = "/home/carson/libs/SAI_output/"
-imgArr = []
+grayLF = np.zeros((101, 101, nRows, nCols), dtype=np.uint8)
 
-terCount = 0
+# Now for depth magiccccc!!!
 
-for iTer in range(1,16):
-    for jTer in range(1,16):
-        lfName = ""
-        if iTer > 9 and jTer > 9:
-            lfName = "CRV_" + str(iTer) + "_" + str(jTer) + ".png"
-        elif iTer < 10 and jTer < 10:
-            lfName = "CRV_0" + str(iTer) + "_0" + str(jTer) + ".png"
-        elif iTer < 10 and jTer > 9:
-            lfName = "CRV_0" + str(iTer) + "_" + str(jTer) + ".png"
-        else:
-            lfName = "CRV_" + str(iTer) + "_0" + str(jTer) + ".png"
+defocusStack = np.zeros((101, 101, 100))
+correspStack = np.zeros((101, 101, 100))
+featuresStack = np.zeros((101, 101, 200))
 
-        lfName = lfLocation + lfName
-        src = cv2.imread(lfName,1)
-        if iTer == 1 and jTer == 1:
-            imgArr = src
-        else:
-            imgArr = np.concatenate((imgArr, src), axis=2)
+depthResolution = 100.0
+deltaDisparity = 21.0
+angRes = nRows
+delta = np.linspace(-deltaDisparity, deltaDisparity, 100)
 
-        # print(str(X[terCount]) + " " + str(Y[terCount]))
-        # print(str(terCount) + " " + str(terCount) + "\n")
-        # cv2.imshow("src", src)
-        # cv2.waitKey(200)
-        terCount = terCount + 1
-    # iTerCount = iTerCount + 1
+for curDepth in delta:
+    saiInd = 0
+    shearedLF = np.zeros((101, 101, nRows * nCols), dtype=np.uint8)
+    for iX in range(0, nCols):
+        for iY in range(0, nRows):
+            grayLF[:, :, iY, iX] = cv2.cvtColor(LF[:, :, :, iY, iX], cv2.COLOR_BGR2GRAY)
 
-print(imgArr.shape)
+            moveX = curDepth * dX
+            moveY = curDepth * dY
+
+            shearedLF[:, :, saiInd] = interp2(grayLF[:, :, iY, iX], moveX[saiInd], moveY[saiInd])
+
+            saiInd = saiInd + 1
+
+
+
+
