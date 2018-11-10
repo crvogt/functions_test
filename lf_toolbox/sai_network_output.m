e98710c851bddbf973db030953abbfcc765b44b7
crvogt <crvogt26@gmail.com>
2018-11-10 18:21:11 +0000

    added some parsing

--- lf_toolbox/sai_network_output.m
+++ lf_toolbox/sai_network_output.m
@@ -3,20 +3,33 @@ clear all
 close all
 
 % Iterate through the scenes:
-baseDir = '/home/carson/libs/SAI_output';
-list = dir([baseDir, '/*'])
-length(list)
-for iter=1:length(list)
-	if list(iter).isdir
-		list(iter) = [];
-		iter = 1;
-	end
-end
-contNames = setdiff({list.name}, {'.', '..'});
-contNames
-for iter=1:length(contNames)
-	fprintf('%s\n', contNames{iter})
-end
+baseDir = '/home/carson/libs/SAI_output/Experiment1';
+list = dir([baseDir, '/*']);
 
-% Iterate through the different exposures:
+list([list.isdir] == 0)=[];
+
+contNames = setdiff({list.name}, {'.', '..'});
+
+for iter=1:2%length(contNames)
+    % Find all Processed Images
+    curDir = strcat(baseDir, '/', contNames{iter});
+    % Getting all processed images
+    curDirList = dir([curDir, '/*_Processed.png']);
+    curDirNames = {curDirList.name};
+    curDirNames = sort(curDirNames);
+    
+    % Create directories based on the number of shutter levels
+    for lightLevel = 1:length(curDirList)
+       folderName = strcat(curDir, '/level_', num2str(lightLevel));
+       [mkdirSuccess, ~, ~] = mkdir(folderName);
+       if mkdirSuccess
+           fprintf('successfully created %s\n', folderName);
+           imgName = strcat(curDir, '/', curDirNames(lightLevel));
+           getSais(imgName, folderName);
+       else
+           fprintf('there was an error creating %s\n', folderName);
+       end
+    end
+    fprintf('\n');  
+end
 
