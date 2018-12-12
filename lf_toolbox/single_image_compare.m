df89e505c912af93430bd995fe92bb1807f06497
crvogt <crvogt26@gmail.com>
2018-12-12 14:01:12 +0000

    single_image_compare

--- lf_toolbox/single_image_compare.m
+++ lf_toolbox/single_image_compare.m
@@ -1,25 +1,51 @@
 % Pull from dark images
 % Compare with enhanced and ref
+clc
+clear all
+close all
 
+base_write_file = '/home/carson/libs/raytrix_gp_data/raytrixImages/low_light_single';
+base_read_file = '/home/carson/libs/raytrix_gp_data/raytrixImages/low_light_proc/devignette'
+num_imgs = 19;
 
+% for iVal = 0:num_imgs
+% 	% dark
+% 	imgPath = strcat(base_read_file, '/', num2str(iVal), '.png');
+% 	LF = get_sub_ap(imgPath);
+
+% 	iter = 13;
+% 	jter = 13;
+% 	img = uint16(squeeze(LF(iter,jter,:,:,:)));
+% 	% img = uint8(squeeze(LF(iter, jter, :, :, :)));
+% 	filename = strcat(base_write_file,'/dark/', num2str(iVal), '.png');
+
+% 	imwrite(img, filename)
+% end
+
+for iVal = 0:num_imgs
+	% enhance
+	imgPath = strcat(base_read_file, '_enhance/', num2str(iVal), '.png');
 	LF = get_sub_ap(imgPath);
 
-	delete(strcat(writeDir, '/*.png'));
-
-	pxStart = 6;
-	pxEnd = 20;
-	pxDist = 1;
-	nCols = pxEnd - pxStart;
-	nRows = nCols;
-
-	icounter = 1;
-	jcounter = 1;
-
-	iter 13;
-	jter 13;
-	% img = uint16(squeeze(LF(iter,jter,:,:,:)));
-	img = uint8(squeeze(LF(iter, jter, :, :, :)));
-	filename = '';
+	iter = 13;
+	jter = 13;
+	img = uint16(squeeze(LF(iter,jter,:,:,:)));
+	% img = uint8(squeeze(LF(iter, jter, :, :, :)));
+	filename = strcat(base_write_file,'/enhance/', num2str(iVal), '.png');
 
 	imwrite(img, filename)
+end
 
+for iVal = 0:num_imgs
+	% ref
+	imgPath = strcat(base_read_file, '_ref/', num2str(iVal), '.png');
+	LF = get_sub_ap(imgPath);
+
+	iter = 13;
+	jter = 13;
+	img = uint16(squeeze(LF(iter,jter,:,:,:)));
+	% img = uint8(squeeze(LF(iter, jter, :, :, :)));
+	filename = strcat(base_write_file,'/ref/', num2str(iVal), '.png');
+
+	imwrite(img, filename)
+end
