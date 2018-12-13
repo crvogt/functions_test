491059f417a13d28c9872ff8404aa6e973511d24
crvogt <crvogt26@gmail.com>
2018-12-13 07:39:40 +0000

    all

---
+++ matlab_plotting/table_test_1.m
@@ -0,0 +1,17 @@
+% Set up an example table.
+LastName = {'Smith';'Johnson';'Williams';'Jones';'Brown'};
+Age = [38;43;38;40;49];
+Height = [71;69;64;67;64];
+Weight = [176;163;131;133;119];
+BloodPressure = [124 93; 109 77; 125 83; 117 75; 122 80];
+T = table(Age,Height,Weight,BloodPressure,'RowNames',LastName);
+% Get the table in string form.
+TString = evalc('disp(T)');
+% Use TeX Markup for bold formatting and underscores.
+TString = strrep(TString,'<strong>','\bf');
+TString = strrep(TString,'</strong>','\rm');
+TString = strrep(TString,'_','\_');
+% Get a fixed-width font.
+FixedWidth = get(0,'FixedWidthFontName');
+% Output the table using the annotation command.
+annotation(gcf,'Textbox','String',TString,'Interpreter','Tex','FontName',FixedWidth,'Units','Normalized','Position',[0 0 1 1]);
