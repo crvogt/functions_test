b23b465a27a0861dea238c1d68b469c15d48ccff
crvogt <crvogt26@gmail.com>
2019-05-10 13:46:58 +0100

    all

--- lf_toolbox/setup_gifs/crawl_sais.m
+++ lf_toolbox/setup_gifs/crawl_sais.m
@@ -1,11 +1,11 @@
 addpath('../')
 
 % imgPath = '/home/carson/libs/gray_to_png/0034035116/plant_processed.png'
-base_loc = '/media/carson/SAMSUNG/var_exp_val_1/2/gif_imgs_light/processed/'
+base_loc = '/media/carson/SAMSUNG/var_exp_val_1/2/hist_eq_denoise/processed/'
 writeDir = strcat(base_loc, 'sais/');
 
-list = dir([base_loc, '*', '.png'])
-cont_names = setdiff({list.name}, {'.', '..'})
+list = dir([base_loc, '*', '.png']);
+cont_names = setdiff({list.name}, {'.', '..'});
 
 for k = 1:length(cont_names)
 	mkdir(strcat(writeDir, num2str(k+19)));
